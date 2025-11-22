3ebf2554e852c964e2e8d5eb96d908fd59ef891a
1436591 <braydenoathout@gmail.com>
2025-11-21 22:12:38 -0500

    Fix FXML resource path to load from resources root

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
         @Override
         public void start(Stage stage) throws Exception {
-            Parent root = FXMLLoader.load(getClass().getResource("SpaceInvader.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/SpaceInvader.fxml"));
 
             Scene scene = new Scene(root);
             stage.setMinWidth(454);
