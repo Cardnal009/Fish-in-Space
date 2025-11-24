d1a56e49c2974745662e8dab9026448874fb70c4
Cassidy <cca5042@uncw.edu>
2025-11-23 23:49:35 -0500

    Add Javadoc comments to Bullet class
    Added documentation comments for the Bullet class and its methods.

--- src/entities/Bullet.java
+++ src/entities/Bullet.java
@@ -6,6 +6,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
+/**
+ * Represents a projectile fired by either the player or an enemy.
+ */
 public class Bullet extends Entity {
 
     private double moveY; // used to move Y value of bullet in update
@@ -13,6 +16,16 @@ public class Bullet extends Entity {
     private Image bulletImage;
     private Entity owner;
 
+    /**
+     * Creates a bullet entity.
+     *
+     * @param x      starting x-position
+     * @param y      starting y-position
+     * @param width  bullet width
+     * @param height bullet height
+     * @param image  bullet sprite 
+     * @param owner  entity that fired the bullet
+     */
     public Bullet(double x, double y, int width, int height, Image image, Entity owner) {
         super(x, y, width, height, image);
         this.bulletImage = image;
@@ -20,6 +33,11 @@ public class Bullet extends Entity {
     }
 
 
+    /**
+     * Draws the bullet on the screen
+     *
+     * @param gc graphics context used to draw
+     */
     @Override
     public void draw(GraphicsContext gc) {
         if (isAlive()) {
@@ -33,6 +51,11 @@ public class Bullet extends Entity {
         }
     }
 
+    /**
+     * Updates bullet movement and marks it dead if out of bounds
+     *
+     * @param delta time in seconds since last update
+     */
     @Override
     public void update(double delta) {
         setY(getY() + (moveY * delta));
@@ -46,23 +69,37 @@ public class Bullet extends Entity {
     }
 
     //getters and setters
-
+    /**
+     * @return vertical movement speed
+     */
     public double getMoveY() {
         return moveY;
     }
 
+    /**
+     * @param moveY vertical movement speed
+     */
     public void setMoveY(double moveY) {
         this.moveY = moveY;
     }
-    
+
+    /**
+     * @return horizontal movement speed
+     */
     public double getMoveX() {
         return moveX;
     }
 
+    /**
+     * @param moveX horizontal movement speed
+     */
     public void setMoveX(double moveX) {
         this.moveX = moveX;
     }
-    
+
+    /**
+     * @return entity that fired this bullet
+     */
     public Entity getOwner() {
         return owner;
     }
