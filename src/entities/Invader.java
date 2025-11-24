a8e0fea325029db4e00f91df9611cf829d00146f
Cardnal009 <traviscain1997@gmail.com>
2025-11-23 23:54:14 -0500

    added comments

--- src/entities/Invader.java
+++ src/entities/Invader.java
@@ -1,22 +1,33 @@
 
 package entities;
 
-import controller.GameController;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 public class Invader extends Entity {
 
-    private boolean invertImage = true;
-    private boolean hasPassedPlayer = false;
+    private boolean invertImage = true; // checks if invader image needs to be inverted
+    private boolean hasPassedPlayer = false; // check for if the invader has went below the player
 
+    /**
+     * Constructor with all arguments
+     * @param x invader x
+     * @param y invader y
+     * @param width invader width
+     * @param height invader height
+     * @param image image of invader
+     */
     public Invader(double x, double y, int width, int height, Image image) {
         super(x, y, width, height, image);
     }
 
+    /**
+     * Draws the invader
+     * @param gc GraphicsContext
+     */
     @Override
     public void draw(GraphicsContext gc) {
-        if (invertImage) {
+        if (invertImage) { // if invader needs to be inverted
             gc.save(); // save current state
             gc.scale(-1,1); // scale to invert
             gc.drawImage(getImage(), -getX() - getWidth(), getY(), getWidth(), getHeight()); // draw at corrected inverted position
@@ -30,14 +41,26 @@ public class Invader extends Entity {
     public void update(double delta) {
     }
 
+    /**
+     *
+     * @param invertImage
+     */
     public void setInvertImage(boolean invertImage) {
         this.invertImage = invertImage;
     }
 
+    /**
+     *
+     * @return boolean
+     */
     public boolean hasPassedPlayer() {
         return hasPassedPlayer;
     }
 
+    /**
+     *
+     * @param hasPassedPlayer
+     */
     public void setHasPassedPlayer(boolean hasPassedPlayer) {
         this.hasPassedPlayer = hasPassedPlayer;
     }
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -9,30 +9,38 @@ import javafx.scene.image.Image;
 public class Player extends Entity {
 
 
-    private boolean isMovingLeft;
-    private boolean isMovingRight;
-    private double moveX;
-    private long lastBullet;
-    private PowerUpManager powerUpManager;
+    private boolean isMovingLeft; // checks if player is moving left
+    private boolean isMovingRight; // checks if player is moving rigght
+    private double moveX; // player x movement
+    private long lastBullet; // cooldown for when player can fire again
+    private PowerUpManager powerUpManager; // placeholder for powerup manager
 
     /**
      * Constructor providing default player image
-     * @param x
-     * @param y
-     * @param width
-     * @param height
-     * @param powerUpManager
+     * @param x x position
+     * @param y y position
+     * @param width player width
+     * @param height player height
+     * @param powerUpManager the powerup manager
      */
     public Player(double x, double y, int width, int height, PowerUpManager powerUpManager) {
         super(x, y, width, height, new Image("assets/invaders/invader.png"));
         this.powerUpManager = powerUpManager;
     }
 
+    /**
+     * Draws the player
+     * @param gc GraphicsContext
+     */
     @Override
     public void draw(GraphicsContext gc) {
         gc.drawImage(getImage(), getX(), getY());
     }
 
+    /**
+     * Updates player movement
+     * @param delta time in seconds since the last update
+     */
     @Override
     public void update(double delta) {
         setX(getX() + (moveX * delta));
@@ -44,38 +52,74 @@ public class Player extends Entity {
         }
     }
 
+    /**
+     *
+     * @return boolean
+     */
     public boolean isMovingLeft() {
         return isMovingLeft;
     }
 
+    /**
+     *
+     * @param movingLeft
+     */
     public void setMovingLeft(boolean movingLeft) {
         isMovingLeft = movingLeft;
     }
 
+    /**
+     *
+     * @return boolean
+     */
     public boolean isMovingRight() {
         return isMovingRight;
     }
 
+    /**
+     *
+     * @param movingRight
+     */
     public void setMovingRight(boolean movingRight) {
         isMovingRight = movingRight;
     }
 
+    /**
+     *
+     * @param moveX
+     */
     public void setMoveX(double moveX) {
         this.moveX = moveX;
     }
 
+    /**
+     *
+     * @return lastBullet
+     */
     public long getLastBullet() {
         return lastBullet;
     }
 
+    /**
+     *
+     * @param bulletTimer
+     */
     public void setLastBullet(long bulletTimer) {
         this.lastBullet = bulletTimer;
     }
 
+    /**
+     *
+     * @return PowerUpManager
+     */
     public PowerUpManager getPowerUpManager() {
         return powerUpManager;
     }
 
+    /**
+     *
+     * @return false if shield absorbs the hit, true if player takes damage
+     */
     public boolean takeDamage() {
         // Returns false if shield absorbed the hit, true if player takes damage
         return !powerUpManager.useShield();
